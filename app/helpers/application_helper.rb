124e4497e4d4122d713d3a5eb3068ce9c09e85a7
sokmesakhiev <sokmesa@gmail.com>
2025-11-06 19:03:07 +0700

    improve header style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def style_focused(controller)
+    return "block py-2 pr-4 pl-3 font-semibold text-indigo-400 hover:text-indigo-300 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" if controller == params[:controller]
+
+    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+  end
 end
